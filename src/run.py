77ad95fc366c1f1f74f46d4137c42ba3f0e07485
Atefe <atefe.ghasemi928@gmail.com>
2026-07-15 12:00:29 +0330

    update run.py

--- src/run.py
+++ src/run.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from io import StringIO
+
 
 st.title(":zap: Streamlit Dashboard")
+
+with st.expander("Statistics"):
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
